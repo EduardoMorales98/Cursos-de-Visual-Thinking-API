234993f412288e8d219b99bba0f42edadbc260c1
EduardoMorales <edalmoes98@gmail.com>
2022-04-29 22:37:12 -0500

    Agregando prueba de unidad con TDD del requerimiento 2 de la clase StudentController

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -4,4 +4,9 @@ describe("Test para StudentController",()=>{
         const allStudents=StudentController.getAllStudents();
         expect(allStudents.length).toBe(51);
     });
+
+    test("2. Consultar los emails de todos los estudiantes que tengan certificación haveCertification",()=>{
+        const studentsWithCertification=StudentController.getStudentsEmailsWithCertification();
+        expect(studentsWithCertification.length).toBe(51);
+    });
 });
